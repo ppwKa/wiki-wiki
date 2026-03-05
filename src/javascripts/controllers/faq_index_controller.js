becef14283f6ef44f73e8afd04af7f843309cd51
pengwei <2435145433@qq.com>
2026-03-05 15:37:53 +0800

    refactor(faq): 更新FAQ图标SVG以提升视觉效果

--- src/javascripts/controllers/faq_index_controller.js
+++ src/javascripts/controllers/faq_index_controller.js
@@ -16,7 +16,7 @@ export default class extends Controller {
     const MOBILE_BREAKPOINT = 768
     const DEFAULT_PLACEHOLDER = "Choose Product Model"
     const PLACEHOLDER_SVG =
-      '<svg viewBox="0 0 24 24"><path d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>'
+      '<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100" fill="none"><rect width="100" height="100" fill="#F5F5F7"/><rect x="10" y="10" width="80" height="80" fill="white"/></svg>'
 
     const filterContainer = this.element.querySelector("#filterContainer")
     const filterHeader = this.element.querySelector("#filterHeader")
